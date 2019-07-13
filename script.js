5fe630303601869c0d2d2d0ca7716c4680836537
Sean Kelly <spkelly18@gmail.com>
2019-07-13 17:18:38 -0600

    refactored audio capture code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 (()=>{
-  
   let analyzer;
   let ac;
   let canvas = document.querySelector('.visualizer');
@@ -7,6 +6,10 @@
   let micIndicator = document.querySelector('.mic_indicator');
   let startButton = document.querySelector('#click-me');
   let ctx = canvas.getContext("2d");
+  const WIDTH = canvas.width;
+  const HEIGHT = canvas.height;
+  const BACKGROUND_COLOR = '#1b283d';
+
 
   console.log(micIndicator);
 
@@ -14,9 +17,7 @@
   let globalStream;
   let animation;
 
-  const WIDTH = canvas.width;
-  const HEIGHT = canvas.height;
-  const BACKGROUND_COLOR = '#1b283d';
+  let playAnimation = false;
 
   let settings = {
     micEnabled:false,
@@ -30,7 +31,8 @@
   updateDisplay();
 
   const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const constraints = {audio:true};
+
+
   ac = new AudioContext();
   analyzer = ac.createAnalyser();
 
@@ -40,84 +42,96 @@
     if(settings.micEnabled){
       stopStream();
       updateDisplay();
-      console.log(settings);
+      playAnimation = false;
     }
     else{
-      
-      ac.resume().then(()=>{
-        console.log('starting audio context');
-        if(navigator.mediaDevices){
-          navigator.mediaDevices.getUserMedia(constraints)
-          .then((stream)=>{
-            settings.micEnabled = true;
-            updateDisplay();
-            console.log(settings);
-            globalStream = stream;
-            var source = ac.createMediaStreamSource(stream);
-            source.connect(analyzer);
-            if(settings.outputSound){
-              analyzer.connect(ac.destination);
-            }
+      ac.resume().then(async()=>{
+        let source = await getMicSource();
+        settings.micEnabled = true;
+        connectAudioNodes(source, analyzer);
+        // outputs sound to speakers
+        // connectAudioNodes(source,analyzer,ac.destination);
+        updateDisplay();
+        vizualizeWaveform(); 
+      })
+    }
 
-            vizualizeWaveform();
+    async function getMicSource(){
+      return new Promise((resolve,reject)=>{
+        if(navigator.mediaDevices){
+          navigator.mediaDevices.getUserMedia({audio:true}).then((stream)=>{
+            let source = ac.createMediaStreamSource(stream);
+            globalStream = stream;
+            resolve(source);
           })
           .catch((e)=>{
-            console.log('an error occured', e.message)
-          });
+            console.log(e.message)
+          })
         }
-       
+      })
+    }
+
+    function connectAudioNodes(...nodes){
+      nodes.forEach((node, currentIndex, nodes)=>{
+        if(currentIndex != nodes.length - 1){
+          node.connect(nodes[currentIndex + 1])
+        }
+        else{
+          console.log('on the last index')
+        }
       })
     }
 
 
     function vizualizeWaveform(){
       analyzer.fftSize = 1024;
+      playAnimation = true;
       var bufferLength = analyzer.fftSize;
-      console.log(bufferLength);
       var dataArray = new Uint8Array(bufferLength);
       
       ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
       
       
-      let drawWaveForm = function(){
-        animation = requestAnimationFrame(drawWaveForm);
-        analyzer.getByteTimeDomainData(dataArray);
-        ctx.fillStyle =  BACKGROUND_COLOR;
-        ctx.fillRect(0, 0,WIDTH, HEIGHT);
-
-        ctx.lineWidth = settings.lineWidth;
-        ctx.strokeStyle = settings.lineColor;
-
-        ctx.beginPath();
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-        for(var i = 0; i < bufferLength; i++) {
-
-          var v = dataArray[i] / 128.0;
-          var y = v * HEIGHT/2;
-
-          // For Debugging purposes
-          // globalX = x;
-          // globalY = y;
-          // globalV = v;
-
-
-          if(i === 0) {
-            ctx.moveTo(x, y);
-          } else {
-            ctx.lineTo(x, y);
+      let animateWaveform = function(){
+        if(playAnimation){
+          requestAnimationFrame(animateWaveform);
+          analyzer.getByteTimeDomainData(dataArray);
+          ctx.fillStyle =  BACKGROUND_COLOR;
+          ctx.fillRect(0, 0,WIDTH, HEIGHT);
+  
+          ctx.lineWidth = settings.lineWidth;
+          ctx.strokeStyle = settings.lineColor;
+  
+          ctx.beginPath();
+          var sliceWidth = WIDTH * 1.0 / bufferLength;
+          var x = 0;
+          for(var i = 0; i < bufferLength; i++) {
+  
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT/2;
+  
+            globalX = x;
+            globalY = y;
+            globalV = v;
+  
+  
+            if(i === 0) {
+              ctx.moveTo(x, y);
+            } else {
+              ctx.lineTo(x, y);
+            }
+  
+            x += sliceWidth;
           }
-
-          x += sliceWidth;
+  
+          ctx.lineTo(WIDTH, HEIGHT/2);
+          ctx.stroke();
         }
-
-        ctx.lineTo(WIDTH, HEIGHT/2);
-        ctx.stroke();
       }
 
 
-      drawWaveForm();
+      animateWaveform();
     }
     
   });
@@ -138,21 +152,24 @@
      track.stop();
    })
    settings.micEnabled = false;
-   clearCanvas();
+   resetCanvas();
   }
 
-  function clearCanvas(){
+  function resetCanvas(){
     if(ctx){
-      ctx.fillRect(0, 0,WIDTH, HEIGHT);
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+      ctx.beginPath();
+      ctx.moveTo(0, HEIGHT/2);
+      ctx.lineTo(WIDTH, HEIGHT/2);
+      ctx.stroke();
     }
     else{
       ctx = document.querySelector(".visualizer").getContext('2d')
-      ctx.fillRect(0, 0,WIDTH, HEIGHT);
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
     }
   }
 
   function updateDisplay(){
-    console.log('here', settings);
     micIndicator.textContent = settings.micEnabled?'enabled':'disabled';
     micIndicator.style.color = settings.micEnabled?'green':'red';
   }
