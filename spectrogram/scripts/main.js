16d890100fe0603859f7aeac6ca1841516eb972c
spkelly <spkelly18@gmail.com>
2019-07-23 15:59:03 -0600

    completed working concept of spectrogram

--- spectrogram/scripts/main.js
+++ spectrogram/scripts/main.js
@@ -2,6 +2,9 @@ window.onload = function(){
   var startButton = document.querySelector('.start');
   var AudioContext = window.AudioContext || window.webkitAudioContext;
 
+
+  // let test = chroma.scale(['red','green']).domain(-140, 0);
+
   startButton.addEventListener('click', ()=>{
     var spectrogram = new Spectrogram(new AudioContext());    
     spectrogram.start();
--- spectrogram/scripts/spectrogram.js
+++ spectrogram/scripts/spectrogram.js
@@ -1,26 +1,29 @@
+
+
 class Spectrogram {
   constructor(audioContext, canvas){
-    // this.analyser.connect(this.audioContext.destination);
     this.audioContext = audioContext;
     this._oscillatorNode = this.audioContext.createOscillator();
     this._analyser = this.audioContext.createAnalyser();
+    this._analyser.fftSize = 2048;
     this._canvas = canvas || document.querySelector('.waterfall')
     this._canvasContext = this._canvas.getContext('2d');
     this._canvasContext.width = window.innerWidth;
     this._canvasContext.height = window.innerHeight;
     this._micSource = null;
     this._currentCol = 0;
+    this._colorScale = chroma.scale(['red','yellow','green','blue']).domain([0.00, -140]);
+    // this.analyser.connect(this.audioContext.destination);
     // this._oscillatorNode.connect(this._analyser);
   }
 
   start(){
     console.log('starting');
-    this._oscillatorNode.frequency.setValueAtTime(1000, this.audioContext.currentTime);
-    this._oscillatorNode.start();
+    // this._oscillatorNode.frequency.setValueAtTime(this.audioContext.currentTime);
+    // this._oscillatorNode.start();
     if(!this._micSource){
       this.connectMic();
     }
-    console.log(this._micSource);
     this._animateSpectrogram();
   }
 
@@ -40,33 +43,38 @@ class Spectrogram {
     
   }
 
-  _draw( data){
-    requestAnimationFrame(this._animateSpectrogram.bind(this));
-    let x = 0;
-    let y = this._canvasContext.canvas.height /2;
-    for(let i = 0; i < data.length; i++){
-      let currentColor = data[i]
-      this._canvasContext.fillStyle = `rgb(${currentColor},0,100)`
-      this._canvasContext.fillRect(this._currentCol,i,2,2);
-    }
-   this._updateCol();
-  }
-  _shiftWaterfall(){
-    let waterfall = this._canvasContext.getImageData(0,0,currentCol,this._canvas.height);
-    this._canvasContext.clearRect(0,0, this._canvas.width, this._canvas.height);
-    this._putImageData(waterfall, 2, 0);
+  _getFreqColor(data){
+    return this._colorScale(data);
   }
 
-  _updateCol(){
-    this._currentCol +=2;
-    if(this._currentCol > this._canvas.width){
-      this._currentCol = 0;
-    };
+  _draw(data){
+    requestAnimationFrame(this._animateSpectrogram.bind(this));
+    this._shiftWaterfall();
+    this._addWaterFallCol(data);
+  }
+
+  _addWaterFallCol(data){
+    for(let i = 0; i < data.length; i++){
+      let currentColor = data[i]
+      // if(currentColor > 220){
+      //   this._canvasContext.fillStyle = `rgb(${currentColor},${currentColor},${currentColor})`;
+      // }
+      // else{
+        
+      // }
+      this._canvasContext.fillStyle = this._getFreqColor(currentColor)
+      this._canvasContext.fillRect(0,i,1,1);
+    }
+  }
+  _shiftWaterfall(){
+    let waterfall = this._canvasContext.getImageData(0,0,this._canvas.width,this._canvas.height);
+    this._canvasContext.clearRect(0,0, this._canvas.width, this._canvas.height);
+    this._canvasContext.putImageData(waterfall, 1, 0);
   }
 
   _animateSpectrogram(){
-    let dataArray = new Uint8Array(this._analyser.frequencyBinCount)
-    this._analyser.getByteFrequencyData(dataArray);
+    let dataArray = new Float32Array(this._analyser.frequencyBinCount);
+    this._analyser.getFloatFrequencyData(dataArray);
     this._draw(dataArray);
   }
 }
